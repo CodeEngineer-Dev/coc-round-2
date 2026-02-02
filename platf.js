b48dadf3d1d58a1acb26119f15a77f153760b624
CodeEngineer-Dev <evanzxliu@gmail.com>
2026-02-02 02:43:55 -0800

    Visible blocks and movement correction

--- platf.js
+++ platf.js
@@ -1,194 +1,233 @@
-const { Block, Platformer } = (function() {
-    // Constants
-    const HVEL = 1; // PLEASE ADJUST AS NECESSARY, I HAVE NOT PLAYTESTED THESE CONSTANTS MAINLY
-    const JUMP = 10; // BECAUSE I HAVE NO IDEA HOW TO ADD A BLOCK TO THE SCENE.
-    const GRAV = -30;
-    const SENS = Math.PI / 250; // mouse sensitivity
+const { Block, Platformer } = (function () {
+  // Constants
+  const HVEL = 2; // PLEASE ADJUST AS NECESSARY, I HAVE NOT PLAYTESTED THESE CONSTANTS MAINLY
+  const JUMP = 10; // BECAUSE I HAVE NO IDEA HOW TO ADD A BLOCK TO THE SCENE.
+  const GRAV = -30;
+  const SENS = Math.PI / 250; // mouse sensitivity
 
-    /** Block, for use in the platforming engine
-     * 
-     * @class Block
-     * @typedef {Block}
+  /** Block, for use in the platforming engine
+   *
+   * @class Block
+   * @typedef {Block}
+   */
+  class Block {
+    /** Creates an instance of Block.
+     *
+     * @constructor
+     * @param {Number} x
+     * @param {Number} y
+     * @param {Number} z
      */
-    class Block {
-        /** Creates an instance of Block.
-         * 
-         * @constructor
-         * @param {Number} x 
-         * @param {Number} y 
-         * @param {Number} z 
-         */
-        constructor(x, y, z, texture) {
-            // Initiates x, y, z, and texture, creates hitbox
-            this.x = x;
-            this.y = y;
-            this.z = z;
-            this.texture = texture;
-            this.hbox = new CubicHitbox(x, y, z, x + 1, y + 1, z + 1);
-        }
+    constructor(x, y, z, rendererScene) {
+      // Initiates x, y, z, and texture, creates hitbox
+      this.x = x;
+      this.y = y;
+      this.z = z;
+      //this.texture = texture;
+      this.hbox = new CubicHitbox(x, y, z, x + 1, y + 1, z + 1);
 
-        /** Adds the block to the scene.
-         * 
-         * @param {Scene} scene 
-         */
-        addToScene(scene) {
-            // ok i actually have no idea how your library works, so i'm going to leave this blank for now
-        }
+      // Assign render component to block
+      this.renderComponent = new RenderComponent("cube/Cube");
+
+      // Since the cube model is drawn around local origin, offset by 1/2 to align to hitbox.
+      this.renderComponent.transform.setPosition(
+        this.hbox.x1 + 0.5,
+        this.hbox.y1 + 0.5,
+        this.hbox.z1 + 0.5,
+      );
+
+      // Since cube model is drawn as a -1 to 1 box, it needs to be half size.
+      this.renderComponent.transform.setScale(0.5, 0.5, 0.5);
+
+      // Add it to the scene
+      this.addToScene(rendererScene);
     }
 
-    /** Player, for use in the platforming engine
-     * 
-     * @class Player
-     * @typedef {Player}
+    /** Adds the block to the scene.
+     *
+     * @param {Scene} scene
      */
-    class Player {
-        static width = 0.7;
-        static height = 1.8; 
+    addToScene(scene) {
+      // addComponent to the renderer's scene
+      scene.addComponent(this.renderComponent);
+    }
+  }
 
-        /** Creates an instance of Block.
-         * 
-         * @constructor
-         * @param {Number} x 
-         * @param {Number} y 
-         * @param {Number} z 
-         */
-        constructor(x, y, z) {
-            // Initiates x, y, z, creates hitbox
-            this.x = x;
-            this.y = y;
-            this.z = z;
-            this.hbox = new CubicHitbox(x, y, z, x + Player.width, y + Player.width, z + Player.height);
-            // Creates x, y, z velocities
-            this.xv = 0;
-            this.yv = 0;
-            this.zv = 0;
-            // Yaw, pitch
-            this.yaw = 0;
-            this.pitch = 0;
-        }
+  /** Player, for use in the platforming engine
+   *
+   * @class Player
+   * @typedef {Player}
+   */
+  class Player {
+    static width = 0.7;
+    static height = 1.8;
 
-        /** Orients camera.
-         * 
-         * @param {Camera} camera 
-         */
-        orient(camera) {
-            // Sets camera position
-            camera.transform.setPosition(
-                this.x + Player.width / 2,
-                this.y + Player.width / 2,
-                this.z + Player.height - 0.3
-            );
-            // Set rotation (0 at the end is roll, but we don't use roll)
-            camera.transform.setRotation(this.pitch, this.yaw, 0);
-        }
-
-        /** Resets hitbox.
-         * 
-         */
-        resetHitbox() {
-            this.hbox.set(this.x, this.y, this.z, this.x + Player.width, this.y + Player.width, this.z + Player.height);
-        }
-
-        /** Takes an array, returns first object to collide with or null.
-         * 
-         * @param {Array} arr 
-         * @returns {*}
-         */
-        touchingArray(arr) {
-            for (const obj of arr) {
-                if (this.hbox.collide(obj.hbox)) {
-                    return obj;
-                }
-            }
-            return null;
-        }
+    /** Creates an instance of Block.
+     *
+     * @constructor
+     * @param {Number} x
+     * @param {Number} y
+     * @param {Number} z
+     */
+    constructor(x, y, z) {
+      // Initiates x, y, z, creates hitbox
+      this.x = x;
+      this.y = y;
+      this.z = z;
+      this.hbox = new CubicHitbox(
+        x,
+        y,
+        z,
+        x + Player.width,
+        y + Player.height - 0.3, // Switched height to y-axis, VERY IMPORTANT!
+        z + Player.width,
+      );
+      // Creates x, y, z velocities
+      this.xv = 0;
+      this.yv = 0;
+      this.zv = 0;
+      // Yaw, pitch
+      this.yaw = 0;
+      this.pitch = 0;
     }
 
-    /** Platformer game class, handles platforming logic
-     * 
-     * @class Platformer
-     * @typedef {Platformer}
+    /** Orients camera.
+     *
+     * @param {Camera} camera
      */
-    class Platformer {
-        /** Creates an instance of Platformer.
-         * 
-         * @param {Number} spawnX 
-         * @param {Number} spawnY 
-         * @param {Number} spawnZ 
-         */
-        constructor(spawnX, spawnY, spawnZ) {
-            this.player = new Player(spawnX, spawnY, spawnZ);
-            this.blocks = [];
-        }
-        /** Adds a block to the blocks.
-         * 
-         * @param {Block} block 
-         */
-        addBlock(block) {
-            this.blocks.push(block);
-        }
-        /** Steps the platformer forward in time.
-         * 
-         */
-        step() {
-            const p = this.player;
-            // Manage x and z velocities, PLEASE ADJUST AS NECESSARY, NOT PLAYTESTED
-            p.xv = 0;
-            p.zv = 0;
-            if (events.KeyW) {
-                p.xv += HVEL * Math.cos(p.yaw);
-                p.zv -= HVEL * Math.sin(p.yaw);
-            }
-            if (events.KeyA) {
-                p.xv -= HVEL * Math.sin(p.yaw);
-                p.zv += HVEL * Math.cos(p.yaw);
-            }
-            if (events.KeyS) {
-                p.xv -= HVEL * Math.cos(p.yaw);
-                p.zv += HVEL * Math.sin(p.yaw);
-            }
-            if (events.KeyD) {
-                p.xv += HVEL * Math.sin(p.yaw);
-                p.zv -= HVEL * Math.cos(p.yaw);
-            }
-            // Handle each axis separately
-            p.x += p.xv * delta;
-            p.resetHitbox();
-            if (p.touchingArray(this.blocks)) {
-                p.x -= p.xv * delta;
-                p.xv = 0;
-                p.resetHitbox();
-            }
-            // Z axis
-            p.z += p.zv * delta;
-            p.resetHitbox();
-            if (p.touchingArray(this.blocks)) {
-                p.z -= p.zv * delta;
-                p.zv = 0;
-                p.resetHitbox();
-            }
-
-            // Y movement
-            p.yv += GRAV * delta;
-            p.y += p.yv * delta;
-            p.resetHitbox();
-            if (p.touchingArray(this.blocks)) {
-                p.y -= p.yv * delta;
-                // Only allow jumps if falling
-                if (events.Space && p.yv < 0) {
-                    p.yv = JUMP;
-                } else {
-                    p.yv = 0;
-                }
-                p.resetHitbox();
-            }
-
-            // Angling
-            p.yaw -= events.dx * SENS;
-            p.pitch -= events.dy * SENS;
-            p.pitch = clamp(p.pitch, -Math.PI / 2, Math.PI / 2);
-        }
+    orient(camera) {
+      this.resetHitbox();
+      // Sets camera position
+      camera.transform.setPosition(
+        this.hbox.x1 + Player.width / 2,
+        this.hbox.y1 + Player.height, // Same change as in the constructor!
+        this.hbox.z1 + Player.width / 2,
+      );
+      // Set rotation (0 at the end is roll, but we don't use roll)
+      camera.transform.setRotation(
+        (this.pitch * 180) / Math.PI,
+        (this.yaw * 180) / Math.PI,
+        0,
+        true,
+      );
     }
 
-    return { Block, Platformer };
+    /** Resets hitbox.
+     *
+     */
+    resetHitbox() {
+      this.hbox.set(
+        this.x,
+        this.y,
+        this.z,
+        this.x + Player.width,
+        this.y + Player.height, // Y axis is up down, not forward back.
+        this.z + Player.width,
+      );
+    }
+
+    /** Takes an array, returns first object to collide with or null.
+     *
+     * @param {Array} arr
+     * @returns {*}
+     */
+    touchingArray(arr) {
+      for (const obj of arr) {
+        if (this.hbox.collide(obj.hbox)) {
+          return obj;
+        }
+      }
+      return null;
+    }
+  }
+
+  /** Platformer game class, handles platforming logic
+   *
+   * @class Platformer
+   * @typedef {Platformer}
+   */
+  class Platformer {
+    /** Creates an instance of Platformer.
+     *
+     * @param {Number} spawnX
+     * @param {Number} spawnY
+     * @param {Number} spawnZ
+     */
+    constructor(spawnX, spawnY, spawnZ) {
+      this.player = new Player(spawnX, spawnY, spawnZ);
+      this.blocks = [];
+    }
+    /** Adds a block to the blocks.
+     *
+     * @param {Block} block
+     */
+    addBlock(block) {
+      this.blocks.push(block);
+    }
+    /** Steps the platformer forward in time.
+     *
+     */
+    step() {
+      const p = this.player;
+      // Manage x and z velocities, PLEASE ADJUST AS NECESSARY, NOT PLAYTESTED
+      p.xv = 0;
+      p.zv = 0;
+
+      // Switched these around until they worked, needed to add negation signs to forward and backwards.
+      if (events.KeyD) {
+        p.xv += HVEL * Math.cos(p.yaw);
+        p.zv -= HVEL * Math.sin(p.yaw);
+      }
+      if (events.KeyS) {
+        p.xv -= HVEL * Math.sin(-p.yaw);
+        p.zv += HVEL * Math.cos(-p.yaw);
+      }
+      if (events.KeyA) {
+        p.xv -= HVEL * Math.cos(p.yaw);
+        p.zv += HVEL * Math.sin(p.yaw);
+      }
+      if (events.KeyW) {
+        p.xv += HVEL * Math.sin(-p.yaw);
+        p.zv -= HVEL * Math.cos(-p.yaw);
+      }
+      // Handle each axis separately
+      p.x += p.xv * delta;
+      p.resetHitbox();
+      if (p.touchingArray(this.blocks)) {
+        p.x -= p.xv * delta;
+        p.xv = 0;
+        p.resetHitbox();
+      }
+      // Z axis
+      p.z += p.zv * delta;
+      p.resetHitbox();
+      if (p.touchingArray(this.blocks)) {
+        p.z -= p.zv * delta;
+        p.zv = 0;
+        p.resetHitbox();
+      }
+
+      // Y movement
+      p.yv += GRAV * delta;
+      p.y += p.yv * delta;
+      p.resetHitbox();
+      if (p.touchingArray(this.blocks)) {
+        p.y -= p.yv * delta;
+        // Only allow jumps if falling
+        if (events.Space && p.yv < 0) {
+          p.yv = JUMP;
+        } else {
+          p.yv = 0;
+        }
+        p.resetHitbox();
+      }
+
+      // Angling
+      p.yaw -= events.dx * SENS;
+      p.pitch -= events.dy * SENS;
+      p.pitch = clamp(p.pitch, -Math.PI / 2, Math.PI / 2);
+    }
+  }
+
+  return { Block, Platformer };
 })();
